894901cfa7d345c7e6b684d726868a897e2d26ec
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-23 13:39:19 -0600

    Updated dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { executeRunCommand } from './vscode/execute-run-command';
+
+
+/**
+ * The channel we'll be writing our output to.
+ */
+const OUTPUT_CHANNEL_NAME = 'just';
+
+/**
+ * The command key for running a just recipe.
+ * 
+ * This needs to match up in two places in our `package.json`.
+ */
+const RUN_RECIPE_COMMAND_KEY = 'just.run';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "just" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('just.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from just!');
-	});
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "justfile" is now active!');
 
-	context.subscriptions.push(disposable);
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(RUN_RECIPE_COMMAND_KEY, async () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        const outputChannel = vscode.window.createOutputChannel(OUTPUT_CHANNEL_NAME);
+        await executeRunCommand(outputChannel);
+    });
+    
+
+    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+/**
+ * Fires when our extension dies.
+ */
+export function deactivate() {
+  // nothing to do
+}
